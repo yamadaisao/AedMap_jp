731804d070c7b3b4c0dda0209d0dc51760ce75fd
Yamada Isao <yamada.isao@gmail.com>
2012-02-14 10:47:52 +0900

    Parcelableを実装

--- src/com/tcf_corp/android/aed/http/MarkerItem.java
+++ src/com/tcf_corp/android/aed/http/MarkerItem.java
@@ -1,5 +1,8 @@
 package com.tcf_corp.android.aed.http;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.OverlayItem;
 
@@ -9,52 +12,114 @@ import com.google.android.maps.OverlayItem;
  * @author yamadaisao
  * 
  */
-public class MarkerItem extends OverlayItem {
+public class MarkerItem extends OverlayItem implements Parcelable {
 
-    /**
-     * コンストラクタ
-     * 
-     * @param id
-     *            ユニークなidです.
-     * @param point
-     *            AED設置情報
-     * @param title
-     *            設置場所
-     * @param snippet
-     *            説明
-     */
-    public MarkerItem(long id, GeoPoint point, String title, String snippet) {
-        super(point, title, snippet);
-        this.id = id;
-    }
+	/** id */
+	public Long id;
+	/** データ元 */
+	public int dataSource;
 
-    public Long id;
-    public String able;
-    public String src;
-    public String spl;
-    public String time;
-    public Long dist;
+	/** 利用可能時間帯 */
+	public String able;
+	/** 情報源 */
+	public String src;
+	/** 特記事項 */
+	public String spl;
+	/** ? */
+	public String time;
+	/** 中心からの距離(m) */
+	public Long dist;
 
-    /** 編集前の情報 */
-    public MarkerItem original;
+	/** アイコンの種類 */
+	public int type;
 
-    @Override
-    public boolean equals(Object obj) {
-        // 引数が自分自身かどうか
-        if (obj == this) {
-            return true;
-        }
-        // 型のチェック、nullチェックも兼ねる
-        if (!(obj instanceof MarkerItem)) {
-            return false;
-        }
-        // このキャストの成功は保証されている
-        MarkerItem item = (MarkerItem) obj;
-        return id != null && id.equals(item.id);
-    }
+	/** 編集前の情報 */
+	public MarkerItem original;
 
-    @Override
-    public int hashCode() {
-        return id.hashCode();
-    }
+	/**
+	 * コンストラクタ
+	 * 
+	 * @param id
+	 *            ユニークなidです.
+	 * @param point
+	 *            AED設置情報
+	 * @param title
+	 *            設置場所
+	 * @param snippet
+	 *            説明
+	 */
+	public MarkerItem(long id, GeoPoint point, String title, String snippet) {
+		super(point, title, snippet);
+		this.id = id;
+	}
+
+	private MarkerItem(Parcel in) {
+		super(new GeoPoint(in.readInt(), in.readInt()), in.readString(), in
+				.readString());
+		id = in.readLong();
+		dataSource = in.readInt();
+		able = in.readString();
+		src = in.readString();
+		spl = in.readString();
+		time = in.readString();
+		dist = in.readLong();
+		type = in.readInt();
+		original = in.readParcelable(null);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// 引数が自分自身かどうか
+		if (obj == this) {
+			return true;
+		}
+		// 型のチェック、nullチェックも兼ねる
+		if (!(obj instanceof MarkerItem)) {
+			return false;
+		}
+		// このキャストの成功は保証されている
+		MarkerItem item = (MarkerItem) obj;
+		return id != null && id.equals(item.id)
+				&& dataSource == item.dataSource;
+	}
+
+	@Override
+	public int hashCode() {
+		return id.hashCode();
+	}
+
+	// implements Parcelable
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeInt(getPoint().getLatitudeE6());
+		dest.writeInt(getPoint().getLongitudeE6());
+		dest.writeString(getTitle());
+		dest.writeString(getSnippet());
+		dest.writeLong(id);
+		dest.writeInt(dataSource);
+		dest.writeString(able);
+		dest.writeString(src);
+		dest.writeString(spl);
+		dest.writeString(time);
+		dest.writeLong(dist);
+		dest.writeInt(type);
+		dest.writeParcelable(original, 0);
+	}
+
+	public static final Parcelable.Creator<MarkerItem> CREATOR = new Parcelable.Creator<MarkerItem>() {
+		@Override
+		public MarkerItem createFromParcel(Parcel in) {
+			return new MarkerItem(in);
+		}
+
+		@Override
+		public MarkerItem[] newArray(int size) {
+			return new MarkerItem[size];
+		}
+	};
 }
