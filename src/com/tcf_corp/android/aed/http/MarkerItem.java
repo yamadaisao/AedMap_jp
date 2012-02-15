2493fe4a73c72abdc84bbd4399972a35b77237bf
yamada isao <yamada.isao@gmail.com>
2012-02-15 22:10:20 +0900

    MarkerItemの主キー変更.よくよく考えたらレイヤーが違うのでidだけで十分.

--- src/com/tcf_corp/android/aed/http/MarkerItem.java
+++ src/com/tcf_corp/android/aed/http/MarkerItem.java
@@ -14,121 +14,114 @@ import com.google.android.maps.OverlayItem;
  */
 public class MarkerItem extends OverlayItem implements Parcelable {
 
-	/** id */
-	public Long id;
-	/** データ元 */
-	public int dataSource;
+    /** id */
+    public Long id;
 
-	/** 利用可能時間帯 */
-	public String able;
-	/** 情報源 */
-	public String src;
-	/** 特記事項 */
-	public String spl;
-	/** parseできなかったりする */
-	public String time;
-	/** 中心からの距離(m) */
-	public Long dist;
+    /** 利用可能時間帯 */
+    public String able;
+    /** 情報源 */
+    public String src;
+    /** 特記事項 */
+    public String spl;
+    /** parseできなかったりする */
+    public String time;
+    /** 中心からの距離(m) */
+    public Long dist;
 
-	/** アイコンの種類 */
-	public int type;
+    /** アイコンの種類 */
+    public int type;
 
-	/** 編集前の情報 */
-	public MarkerItem original;
+    /** 編集前の情報 */
+    public MarkerItem original;
 
-	/** 編集balloon との受け渡し用. */
-	public String editTitle;
-	/** 編集balloon との受け渡し用. */
-	public String editSnippet;
+    /** 編集balloon との受け渡し用. */
+    public String editTitle;
+    /** 編集balloon との受け渡し用. */
+    public String editSnippet;
 
-	/**
-	 * コンストラクタ
-	 * 
-	 * @param id
-	 *            ユニークなidです.
-	 * @param point
-	 *            AED設置情報
-	 * @param title
-	 *            設置場所
-	 * @param snippet
-	 *            説明
-	 */
-	public MarkerItem(long id, GeoPoint point, String title, String snippet) {
-		super(point, title, snippet);
-		this.id = id;
-	}
+    /**
+     * コンストラクタ
+     * 
+     * @param id
+     *            ユニークなidです.
+     * @param point
+     *            AED設置情報
+     * @param title
+     *            設置場所
+     * @param snippet
+     *            説明
+     */
+    public MarkerItem(long id, GeoPoint point, String title, String snippet) {
+        super(point, title, snippet);
+        this.id = id;
+    }
 
-	private MarkerItem(Parcel in) {
-		super(new GeoPoint(in.readInt(), in.readInt()), in.readString(), in
-				.readString());
-		id = in.readLong();
-		dataSource = in.readInt();
-		able = in.readString();
-		src = in.readString();
-		spl = in.readString();
-		time = in.readString();
-		dist = in.readLong();
-		type = in.readInt();
-		original = in.readParcelable(null);
-	}
+    private MarkerItem(Parcel in) {
+        super(new GeoPoint(in.readInt(), in.readInt()), in.readString(), in.readString());
+        id = in.readLong();
+        able = in.readString();
+        src = in.readString();
+        spl = in.readString();
+        time = in.readString();
+        dist = in.readLong();
+        type = in.readInt();
+        original = in.readParcelable(null);
+    }
 
-	// override equals/hashCode
-	@Override
-	public boolean equals(Object obj) {
-		// 引数が自分自身かどうか
-		if (obj == this) {
-			return true;
-		}
-		// 型のチェック、nullチェックも兼ねる
-		if (!(obj instanceof MarkerItem)) {
-			return false;
-		}
-		// このキャストの成功は保証されている
-		MarkerItem item = (MarkerItem) obj;
-		return id != null && id.equals(item.id)
-				&& dataSource == item.dataSource;
-	}
+    // override equals/hashCode
+    @Override
+    public boolean equals(Object obj) {
+        // 引数が自分自身かどうか
+        if (obj == this) {
+            return true;
+        }
+        // 型のチェック、nullチェックも兼ねる
+        if (!(obj instanceof MarkerItem)) {
+            return false;
+        }
+        // このキャストの成功は保証されている
+        MarkerItem item = (MarkerItem) obj;
+        return id != null && id.equals(item.id);
+    }
 
-	@Override
-	public int hashCode() {
-		int hash = 1;
-		hash = hash * 31 + id.hashCode();
-		hash = hash * 31 + Integer.bitCount(dataSource);
-		return hash;
-	}
+    @Override
+    public int hashCode() {
+        int hash = 1;
+        hash = hash * 31 + id.hashCode();
+        return hash;
+    }
 
-	// implements Parcelable
-	@Override
-	public int describeContents() {
-		return 0;
-	}
+    // implements Parcelable
+    @Override
+    public int describeContents() {
+        return 0;
+    }
 
-	@Override
-	public void writeToParcel(Parcel dest, int flags) {
-		dest.writeInt(getPoint().getLatitudeE6());
-		dest.writeInt(getPoint().getLongitudeE6());
-		dest.writeString(getTitle());
-		dest.writeString(getSnippet());
-		dest.writeLong(id);
-		dest.writeInt(dataSource);
-		dest.writeString(able);
-		dest.writeString(src);
-		dest.writeString(spl);
-		dest.writeString(time);
-		dest.writeLong(dist);
-		dest.writeInt(type);
-		dest.writeParcelable(original, 0);
-	}
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(getPoint().getLatitudeE6());
+        dest.writeInt(getPoint().getLongitudeE6());
+        dest.writeString(getTitle());
+        dest.writeString(getSnippet());
+        dest.writeLong(id);
+        dest.writeString(able);
+        dest.writeString(src);
+        dest.writeString(spl);
+        dest.writeString(time);
+        dest.writeLong(dist);
+        dest.writeInt(type);
+        dest.writeParcelable(original, 0);
+    }
 
-	public static final Parcelable.Creator<MarkerItem> CREATOR = new Parcelable.Creator<MarkerItem>() {
-		@Override
-		public MarkerItem createFromParcel(Parcel in) {
-			return new MarkerItem(in);
-		}
+    public static final Parcelable.Creator<MarkerItem> CREATOR = new Parcelable.Creator<MarkerItem>() {
+        @Override
+        public MarkerItem createFromParcel(Parcel in) {
+            return new MarkerItem(in);
+        }
 
-		@Override
-		public MarkerItem[] newArray(int size) {
-			return new MarkerItem[size];
-		}
-	};
+        @Override
+        public MarkerItem[] newArray(int size) {
+            return new MarkerItem[size];
+        }
+    };
 }
